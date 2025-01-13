886ca179aac942d0a8b5d30139f6edbb161473d7
fabiandiaz416 <diazfabian99@icloud.com>
2025-01-13 18:22:42 -0300

    add player services funcs

---
+++ backend/src/services/playerService.ts
@@ -0,0 +1,28 @@
+// Interface to describe a player
+
+interface Player{
+    id: string;
+    totalChars: number;
+    isIA: boolean;
+    isEliminated: boolean; // Indicate if was eliminated because of votation of because it didn't fullfill the minimum 20 chars
+}
+
+// Create a new player
+export const createPlayer = (playerId: string): Player => { 
+    return  {
+        id: playerId,
+        totalChars: 0,
+        isIA: false,
+        isEliminated: false
+    };
+};
+// Increment the amount of chars while he is sendind messages
+// If he reach 20 it does not keep counting
+export const addCharsToPlayers = (player: Player, charCount: number) => { 
+    player.totalChars += charCount;
+}
+
+export const eliminatePlayer = (player: Player) => {
+    player.isEliminated = true;
+}
+
