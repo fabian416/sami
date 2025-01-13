532091a968e6d89d29daa849bcd52cd5fe66643d
fabiandiaz416 <diazfabian99@icloud.com>
2025-01-13 18:04:52 -0300

    add end round function

--- backend/src/services/gameService.ts
+++ backend/src/services/gameService.ts
@@ -13,6 +13,40 @@ export const createNewGame = (roomId: string) => {
     return newGame;
 };
 
+export const startRound = (roomId: string) => { 
+    const game = games[roomId];
+    if (!game) {
+        return null; // Will need to show a proper error
+    }
+
+    // Logic to start the round
+    if (game.status === 'waiting') {
+        game.status === 'active';
+    } else if (game.sattus === 'active') {
+        // Next round
+        game.round +=1;
+    }
+
+    // Apply the rules
+    // clean votes after voting phase
+    // verify if someone got kicked out, etc
+
+    return game;
+}
+
+const endRound = (roomId: string) =>  {
+    const game = games[roomId];
+    if(!game) {
+        return null;
+    }
+    // Apply the end of the round 
+    // Calculate results
+    // delete players
+    // Change status from waiting to finished
+    game.status = 'finished';
+    return game;
+}
+
 // Get a Match by his ID
 export const getGameById = (roomId: string) => {
     return games[roomId] || null ;
