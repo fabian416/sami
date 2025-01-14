0693943ee72722ec8b1743b394cbb5790dc92d76
fabiandiaz416 <diazfabian99@icloud.com>
2025-01-13 21:29:05 -0300

    add return in the res

--- backend/src/controllers/gameController.ts
+++ backend/src/controllers/gameController.ts
@@ -10,8 +10,22 @@ export const createGame = (req: Request, res: Response) => {
         return res.status(400).json({message: 'The ID of the room is obligatory!!'});   
     }
     const game = createNewGame(roomId);
-    res.status(201).json({message: 'Match succesfully created, have fun!', game});
+    return res.status(201).json({message: 'Match succesfully created, have fun!', game});
 };
+
+export const castVote = (req: Request, res: Response) => {
+    const {roomId, voterId, votedId} = req.body;
+    // Validation
+    if (!roomId || !voterId || !votedId) {
+        return res.status(400).json({message: 'Incomplete data'});
+    }
+
+    const success = recordVote(roomId, voterId, votedId);
+    if (!success){
+        return res.status(400).json({message: 'Failed to register the Vote'});
+    }
+    return res.status(200).json({message: 'Success registering the Vote'});
+}
 
 // Get info of the match
 export const getGame = (req:Request, res:Response) => {
@@ -22,5 +36,5 @@ export const getGame = (req:Request, res:Response) => {
         return res.status(404).json({message: 'Match not founf'});
     }
 
-    res.status(200).json(game);
+    return res.status(200).json(game);
 };
