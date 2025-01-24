456a8ae2f3ee5035408b5168b7c90a4c0b6f5fd0
lucholeonel <carrenoluciano@gmail.com>
2025-01-24 03:05:39 -0300

    wip

--- packages/nextjs/components/sami/ModalFinished.tsx
+++ packages/nextjs/components/sami/ModalFinished.tsx
@@ -7,12 +7,14 @@ export const ModalFinished = ({ winner }: any) => {
       <div className="bg-base-100 rounded-2xl items-center justify-center">
         <div className="flex items-center justify-center text-center flex-col gap-2 py-8 px-16">
           {winner === "humans" ? <strong>Humans won</strong> : <strong>SAMI won</strong>}
-          <div className="w-16 h-16">
+          <div className="w-48 h-48">
             {winner === "humans" ? (
-              <UserGroupIcon />
+              <div className="flex relative w-48 h-48">
+                <Image alt="SE2 logo" className="cursor-pointer" fill src="/humans-won.webp" />
+              </div>
             ) : (
-              <div className="flex relative w-16 h-16">
-                <Image alt="SE2 logo" className="cursor-pointer" fill src="/sami.png" />
+              <div className="flex relative w-48 h-48">
+                <Image alt="SE2 logo" className="cursor-pointer" fill src="/sami-won.webp" />
               </div>
             )}
           </div>
