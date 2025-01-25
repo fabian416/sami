12418c5b9a52e5830953a1779d17f87b489e8a85
lucholeonel <carrenoluciano@gmail.com>
2025-01-25 04:18:55 -0300

    change cards colors

--- packages/nextjs/components/sami/ChooseGame.tsx
+++ packages/nextjs/components/sami/ChooseGame.tsx
@@ -67,22 +67,24 @@ export const ChooseGame = ({ showGame }: any) => {
     <>
       {loading && <ModalWaitingForPlayers />}
       <div className="flex md:flex-row flex-col justify-evenly items-center w-full gap-10 md:gap-8">
-        <div className="card bg-gray-500 text-white max-w-sm md:w-96 shadow-xl mx-4">
+        <div className="card bg-[#FF6F61] dark:bg-[#FF6F61] text-[#222] max-w-sm md:w-96 shadow-xl mx-4">
           <div className="card-body">
             <h2 className="card-title">Enter game!</h2>
-            <p>Pay a 2 USDC fee to participate in the next round of SAMI!</p>
+            <p className="h-10">Pay a 2 USDC fee to participate in the next round of SAMI!</p>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary bg-gray-700 text-white border-0">Pay 2 USDC</button>
+              <button className="btn btn-primary bg-[#E45750] hover:bg-[#D64944] dark:bg-[#E45750] text-white border-0">
+                Pay 2 USDC
+              </button>
             </div>
           </div>
         </div>
-        <div className="card bg-cyan-700 text-white max-w-sm md:w-96 shadow-xl mx-4">
+        <div className="card bg-[#40E0D0] dark:bg-[#40E0D0] text-[#222] max-w-sm md:w-96 shadow-xl mx-4">
           <div className="card-body">
             <h2 className="card-title">Enter free game!</h2>
-            <p>Participate free in the next round of SAMI!</p>
+            <p className="h-10">Participate free in the next round of SAMI!</p>
             <div className="card-actions justify-end">
               <button
-                className="btn btn-primary bg-red-600 hover:bg-red-700 text-white border-0"
+                className="btn btn-primary bg-[#1CA297] hover:bg-[#1B9086] dark:bg-[#249C8E] dark:hover:bg-[#1B9086] text-white border-0"
                 onClick={handleEnterGame}
                 disabled={loading}
               >
