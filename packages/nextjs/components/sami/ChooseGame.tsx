97bf3ae34f033d7f015cd3668143ca3dc12ebf4a
Lulox <lulox.eth@protonmail.com>
2025-02-15 02:34:19 +0000

    Fix size of reserves

--- packages/nextjs/components/sami/ChooseGame.tsx
+++ packages/nextjs/components/sami/ChooseGame.tsx
@@ -254,13 +254,13 @@ export const ChooseGame = ({ showGame }: any) => {
             </div>
           </div>
         </div>
-        <div className="sami-title text-center mt-4 text-4xl bg-[#B2CB00] hover:bg-[#A1CA00] glow-yellow text-black p-2 rounded-lg">
+        <div className="sami-title text-center mt-4 text-lg bg-[#B2CB00] hover:bg-[#A1CA00] glow-yellow text-black p-2 rounded-lg">
           SAMI Reserves: {samiBalance ? Number(samiBalance) / 1e18 : "0"}&nbsp;
           <Image
             src="/mode.png"
             alt="MODE Network Logo"
-            width="40"
-            height="40"
+            width="25"
+            height="25"
             className="inline-block align-top" // Add this to align the image with the text
           />
           {/* SAMI Reserves: {samiBalance ? (Number(samiBalance) / 1e18).toFixed(2) : "0.00"} */}
