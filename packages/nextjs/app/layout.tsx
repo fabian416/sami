f681da4206597ba3ca84881de751f720eeb814bf
lucholeonel <carrenoluciano@gmail.com>
2025-02-15 21:16:43 -0300

    add open graph

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -11,7 +11,6 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html suppressHydrationWarning>
       <head>
-        {" "}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
@@ -24,6 +23,19 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
           href="https://fonts.googleapis.com/css2?family=Audiowide&family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         />
+
+        <meta property="og:title" content="SAMI - Play against AI" />
+        <meta property="og:description" content="Bet on who do you think is the AI and earn a profit." />
+        <meta property="og:image" content="https://playsami.fun/thumbnail.jpg" />
+        <meta property="og:url" content="https://playsami.fun" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="SAMI" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="SAMI - Play against AI" />
+        <meta name="twitter:description" content="Bet on who do you think is the AI and earn a profit." />
+        <meta name="twitter:image" content="https://playsami.fun/thumbnail.jpg" />
+        <meta name="twitter:site" content="@sami_ai_agent" />
       </head>
       <body>
         <ThemeProvider enableSystem>
