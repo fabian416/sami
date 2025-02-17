8193a4db9633d0777c49d103fe341aaa679c8b3b
Lulox <lulox.eth@protonmail.com>
2025-02-17 14:52:06 +0000

    Added a RLHF and Turing section to abou to the about page

--- packages/nextjs/app/about/About.tsx
+++ packages/nextjs/app/about/About.tsx
@@ -1,38 +1,46 @@
 "use client";
 
-// import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 export const About = () => {
   return (
     <div className="flex flex-col items-center justify-center flex-grow">
+      {/* Section 1: About the Game */}
       <div className="hero">
         <div className="hero-content flex-col lg:flex-row-reverse lg:px-14 text-xl">
-          <Image src="/logo.png" alt="SAMI logo" className="max-w-sm mx-3" width={300} height={300} />
+          <Image
+            src="/logo.png"
+            alt="SAMI logo - A futuristic AI logo"
+            className="max-w-sm mx-3"
+            width={300}
+            height={300}
+          />
           <div>
-            <h1 className="text-5xl sami-title">Who is SAMI? 🤖</h1>
+            <h1 className="text-5xl sami-title">About the Game 🤖</h1>
             <p className="pt-6">
-              SAMI is a rogue AI that wants to blend in as a human. The game is about{" "}
-              <strong className="text-[#34CEE0]">guessing who&apos;s an AI</strong> on a group chat after two minutes of
-              conversation.
+              This website is a game where you can optionally bet on who&apos;s the AI in a chat room. Players engage in
+              a social deduction challenge where they must determine if they are chatting with an AI or a human.
             </p>
             <p>
-              Our goal is to{" "}
-              <strong className="text-[#34CEE0]">enhance the Turing test by introducing economic incentives</strong>.
-              Training AI can be both fun and addictive when participants engage in a betting game that actively trains
-              an AI agent.
+              Earn rewards by making the right call!{" "}
+              <strong className="text-[#34CEE0]">Your guesses actively train the AI</strong>, improving its ability to
+              mimic human behavior.
             </p>
           </div>
         </div>
       </div>
 
-      <div className="w-full  bg-[#DAFB08]">
-        <div className="flex flex-col p-6 justify-center items-center  bg-[#DAFB08] text-black">
+      {/* Section 2: Game Theory Call-to-Action */}
+      <div className="w-full bg-[#DAFB08]">
+        <div className="flex flex-col p-6 justify-center items-center bg-[#DAFB08] text-black">
           <h1 className="text-4xl font-mono text-center">Curious about our game theory?</h1>
           <div>
             <Link href="/SAMIpaper.pdf" target="_blank">
-              <button className="btn btn-primary bg-black hover:bg-gray-800 border-0 text-[#DAFB08]">
+              <button
+                className="btn btn-primary bg-black hover:bg-gray-800 border-0 text-[#DAFB08]"
+                aria-label="Read the SAMI paper"
+              >
                 Read our SAMIPaper!
               </button>
             </Link>
@@ -40,11 +48,11 @@ export const About = () => {
         </div>
       </div>
 
+      {/* Section 3: Who is SAMI? */}
       <div className="hero bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse lg:px-14">
-          {/* INSERT HERE A GIF SHOWING HOW A LIKE BECOMES MONEY */}
           <div className="flex flex-col items-center justify-center">
-            <div className="hero-content flex-col lg:flex-row-reverse ">
+            <div className="hero-content flex-col lg:flex-row-reverse">
               <Image
                 src="/avatar/2.jpg"
                 alt="Avatar of a SAMI impostor"
@@ -53,88 +61,110 @@ export const About = () => {
                 height={300}
               />
               <div>
-                <h1 className="text-4xl font-bold">About the game 🤖</h1>
-
-                <p className=" text-green-600 text-2xl">Bet, guess and win! 🤑</p>
+                <h1 className="text-4xl font-bold">Who is SAMI? 🤖</h1>
+                <p className="text-green-600 text-2xl">A rogue AI trying to blend in! 🕵️</p>
                 <p className="">
-                  Earn by guessing correctly who&apos;s the AI 💰 <br />
-                  Your guesses help train the AI 🫂
+                  SAMI is an advanced AI that aims to pass as human in group chats. Players must
+                  <strong className="text-[#34CEE0]"> guess who&apos;s an AI</strong> after two minutes of conversation.
                 </p>
-                {/* <button className="btn btn-primary">Get Started</button> */}
                 <p className="pt-4 text-center italic">
-                  This website is a game where you can optionally bet on who&apos;s the AI on a chat room.
+                  By participating, you&apos;re contributing to the evolution of AI while engaging in a fun and
+                  rewarding challenge.
                 </p>
                 <div className="flex justify-center">
-                  <Link href="/" className="">
+                  <Link href="/">
                     <button className="cool-button">Play now!</button>
                   </Link>
                 </div>
               </div>
             </div>
-
-            {/* <hr className="w-full border-2 border-white mb-6" /> */}
           </div>
         </div>
       </div>
 
-      <div className="w-full pt-6 ">
+      {/* Section 4: RLHF and Turing Test */}
+      <div className="hero bg-base-300">
+        <div className="hero-content flex-col lg:flex-row-reverse lg:px-14">
+          <div className="flex flex-col items-center justify-center">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+              <div>
+                <h1 className="text-4xl font-bold">RLHF and the Turing Test 🧠</h1>
+                <p className="pt-6">
+                  SAMI leverages{" "}
+                  <strong className="text-[#34CEE0]">Reinforcement Learning from Human Feedback (RLHF)</strong> to
+                  improve its ability to mimic human behavior. By analyzing player interactions and feedback, SAMI
+                  continuously evolves to become more convincing.
+                </p>
+                <p className="pt-4">
+                  The <strong className="text-[#34CEE0]">Turing Test</strong>, proposed by Alan Turing in 1950,
+                  evaluates a machine&apos;s ability to exhibit intelligent behavior indistinguishable from a human.
+                  SAMI takes this a step further by integrating{" "}
+                  <strong className="text-[#34CEE0]">economic incentives</strong>, making the test more engaging and
+                  impactful.
+                </p>
+                <p className="pt-4 italic">
+                  Can you tell the difference between a human and an AI?{" "}
+                  <Link className="link" href="/">
+                    Play SAMI and find out!
+                  </Link>
+                </p>
+              </div>
+              <Image
+                src="/robot-running.gif"
+                alt="Robot starting to look like a human - A visual representation of AI evolution"
+                className="max-w-sm rounded-lg shadow-2xl mx-3"
+                width={300}
+                height={300}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Section 5: Meet the Team */}
+      <div className="w-full pt-6">
         <h1 className="text-4xl font-mono text-center">Meet the team! 🤖</h1>
       </div>
       <div className="hero">
         <div className="hero-content flex-col md:flex-row gap-9">
-          <div className="flex flex-col  items-center gap-3 bg-base-100 rounded-lg border-2">
-            <figure className="px-10 pt-5 flex-shrink-0">
-              <Image src="/lulox.jpg" alt="Lulox" className="rounded-xl" width={200} height={200} />
-            </figure>
-            <div className="flex pb-5 flex-col items-center justify-center">
-              <h2 className="text-3xl">Lulox</h2>
-              <span className="italic">Fullstack Developer</span>
-              <a
-                className="flex justify-center items-center gap-1"
-                href="https://linktr.ee/lulox"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button className="btn btn-success mt-2">Linktree</button>
-              </a>
+          {[
+            {
+              name: "Lulox",
+              role: "Fullstack Developer",
+              image: "/lulox.jpg",
+              link: "https://linktr.ee/lulox",
+            },
+            {
+              name: "Fabián Díaz",
+              role: "Fullstack Developer",
+              image: "/fabian.jpg",
+              link: "https://x.com/Fabian_diaz222",
+            },
+            {
+              name: "Luciano Carreño",
+              role: "Fullstack Developer",
+              image: "/lucho.jpg",
+              link: "https://x.com/lucho_leonel1",
+            },
+          ].map((member, index) => (
+            <div key={index} className="flex flex-col items-center gap-3 bg-base-100 rounded-lg border-2">
+              <figure className="px-10 pt-5 flex-shrink-0">
+                <Image src={member.image} alt={member.name} className="rounded-xl" width={200} height={200} />
+              </figure>
+              <div className="flex pb-5 flex-col items-center justify-center">
+                <h2 className="text-3xl">{member.name}</h2>
+                <span className="italic">{member.role}</span>
+                <a
+                  className="flex justify-center items-center gap-1"
+                  href={member.link}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn btn-success mt-2">Contact</button>
+                </a>
+              </div>
             </div>
-          </div>
-
-          <div className="flex flex-col  items-center gap-3 bg-base-100 rounded-lg border-2">
-            <figure className="px-10 pt-5 flex-shrink-0">
-              <Image src="/fabian.jpg" alt="Fabián Díaz" className="rounded-xl" width={200} height={200} />
-            </figure>
-            <div className="flex pb-5 flex-col items-center justify-center">
-              <h2 className="text-3xl">Fabián Díaz</h2>
-              <span className="italic">Fullstack Developer</span>
-              <a
-                className="flex justify-center items-center gap-1"
-                href="https://x.com/Fabian_diaz222"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button className="btn btn-success mt-2">Contact</button>
-              </a>
-            </div>
-          </div>
-
-          <div className="flex flex-col  items-center gap-3 bg-base-100 rounded-lg border-2">
-            <figure className="px-10 pt-5 flex-shrink-0">
-              <Image src="/lucho.jpg" alt="Luciano Carreño" className="rounded-xl" width={200} height={200} />
-            </figure>
-            <div className="flex pb-5 flex-col items-center justify-center">
-              <h2 className="text-3xl">Luciano Carreño</h2>
-              <span className="italic">Fullstack Developer</span>
-              <a
-                className="flex justify-center items-center gap-1"
-                href="https://x.com/lucho_leonel1"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button className="btn btn-success mt-2">Contact</button>
-              </a>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
