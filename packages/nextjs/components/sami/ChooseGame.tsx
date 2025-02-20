5eb66eb5efa0c2c8ffbbcd899e181d14b3464700
lucholeonel <carrenoluciano@gmail.com>
2025-02-20 12:02:02 -0300

    fix front to wait for transaction confirmation

--- packages/nextjs/components/sami/ChooseGame.tsx
+++ packages/nextjs/components/sami/ChooseGame.tsx
@@ -6,7 +6,12 @@ import { ModalWaitingForTransaction } from "./ModalWaitingForTransaction";
 import { v4 as uuidv4 } from "uuid";
 import { useAccount } from "wagmi";
 import { useSocket } from "~~/app/socketContext";
-import { useDeployedContractInfo, useScaffoldReadContract, useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
+import {
+  useDeployedContractInfo,
+  useScaffoldReadContract,
+  useScaffoldWriteContract,
+  useTransactor,
+} from "~~/hooks/scaffold-eth";
 import { notification } from "~~/utils/scaffold-eth";
 
 interface Player {
@@ -26,6 +31,7 @@ export const ChooseGame = ({ showGame }: any) => {
   const [localAllowance, setLocalAllowance] = useState<bigint | null>(null);
   const { socket, isConnected, playerId, setPlayerId, setPlayerIndex, setRoomId } = useSocket();
   const { address: connectedAddress } = useAccount();
+  const transactor = useTransactor();
   const { writeContractAsync: USDCwriteContractAsync } = useScaffoldWriteContract({ contractName: "USDC" });
   const { writeContractAsync: simpleSAMIWriteContractAsync } = useScaffoldWriteContract({
     contractName: "USDCSimpleSAMI",
@@ -75,15 +81,21 @@ export const ChooseGame = ({ showGame }: any) => {
 
     setLoadingApprove(true);
     try {
-      const contractResponse = await USDCwriteContractAsync({
-        functionName: "approve",
-        args: [simpleSAMIContractData?.address, BigInt(1 * DECIMALS)],
-      });
+      const transactionHash = await transactor(
+        async () => {
+          const txHash = await USDCwriteContractAsync({
+            functionName: "approve",
+            args: [simpleSAMIContractData?.address, BigInt(1 * DECIMALS)],
+          });
+          if (!txHash) throw new Error("Transaction was not sent properly.");
+          return txHash;
+        },
+        { blockConfirmations: 1 },
+      );
 
-      if (contractResponse) {
-        setLocalAllowance(BigInt(1 * DECIMALS));
-        notification.success("Allowance increased successfully!");
-      }
+      if (!transactionHash) throw new Error("Transaction was not sent properly.");
+      setLocalAllowance(BigInt(1 * DECIMALS));
+      console.log("Transaction confirmed:", transactionHash);
     } catch (error) {
       console.error("Error increasing allowance:", error);
       notification.error("Increasing allowance failed, please try again.");
@@ -99,39 +111,43 @@ export const ChooseGame = ({ showGame }: any) => {
 
     setLoadingBet(true);
     try {
-      const contractResponse = await simpleSAMIWriteContractAsync({
-        functionName: "enterGame",
-        args: undefined,
-      });
+      const transactionHash = await transactor(
+        async () => {
+          const txHash = await simpleSAMIWriteContractAsync({
+            functionName: "enterGame",
+            args: undefined,
+          });
+          if (!txHash) throw new Error("Transaction was not sent properly.");
+          return txHash;
+        },
+        { blockConfirmations: 1 },
+      );
 
-      console.log("contractResponse", contractResponse);
+      if (!transactionHash) throw new Error("Transaction was not sent properly.");
+      console.log("Transaction confirmed:", transactionHash);
 
-      if (contractResponse) {
-        notification.success("Paid for a game of SAMI successfully!");
+      // 🔹 Generar un ID único para el jugador
+      const randomPlayerId = uuidv4();
+      setPlayerId(randomPlayerId);
+      setIsBetGame(true);
 
-        // 🔹 Generar un ID único para el jugador
-        const randomPlayerId = uuidv4();
-        setPlayerId(randomPlayerId);
-        setIsBetGame(true);
+      setLoading(true);
 
-        setLoading(true);
+      // 🔥 **Emitir evento al backend para unirse a la partida**
+      socket.emit(
+        "createOrJoinGame",
+        { playerId: randomPlayerId, isBetGame: true }, // ✅ Se marca como partida de apuestas
+        (response: { success: boolean; roomId: string }) => {
+          setLoading(false);
 
-        // 🔥 **Emitir evento al backend para unirse a la partida**
-        socket.emit(
-          "createOrJoinGame",
-          { playerId: randomPlayerId, isBetGame: true }, // ✅ Se marca como partida de apuestas
-          (response: { success: boolean; roomId: string }) => {
-            setLoading(false);
-
-            if (response.success && response.roomId) {
-              setRoomId(response.roomId); // ✅ Almacena `roomId` en el contexto
-            } else {
-              console.error("Failed to join game:", response);
-              alert("Error joining the game. Please try again.");
-            }
-          },
-        );
-      }
+          if (response.success && response.roomId) {
+            setRoomId(response.roomId); // ✅ Almacena `roomId` en el contexto
+          } else {
+            console.error("Failed to join game:", response);
+            alert("Error joining the game. Please try again.");
+          }
+        },
+      );
     } catch (error) {
       console.error("Error buying ticket:", error);
       notification.error("Buying ticket failed, please try again.");
