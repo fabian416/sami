c8155359c5d569c2e55d01bd83efb02ad449b5dc
lucholeonel <carrenoluciano@gmail.com>
2025-09-16 05:07:24 -0300

    wip

--- packages/frontend/src/contracts/externalContracts.ts
+++ packages/frontend/src/contracts/externalContracts.ts
@@ -14,7 +14,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const externalContracts = {
   137: {
     USDC: {
-      address: "0x3c499C542cEF5E3811E1192Ce70d8cC03d5c3359",
+      address: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
       abi: [
         {
           anonymous: false,
